f1b61283fe0f74661e5bc5fba663c579eca5372e
signingvardo <signingvardo@gmail.com>
2025-06-03 17:10:21 +0100

    Update package dependencies for email components and refactor email handling; improved variable naming for clarity in email template.

--- emails/index.tsx
+++ emails/index.tsx
@@ -1,3 +1,4 @@
+import { ContactData } from "@/components/Formulaire";
 import {
   Html,
   Head,
@@ -7,18 +8,19 @@ import {
   Preview,
 } from "@react-email/components";
 import * as React from "react";
-import { ContactData } from "@/components/HomeSection3";
 
 export default function Email({
-  message,
-  name,
-  phone_number,
+  Message,
+  Nom,
+  Prénom,
+  Sujet,
+  Téléphone,
   originUrl,
 }: ContactData & { originUrl: string }) {
   return (
     <Tailwind>
       <Preview>
-        {message?.length > 90 ? `${message.slice(0, 90)}...` : message}
+        {Message?.length > 90 ? `${Message.slice(0, 90)}...` : Message}
       </Preview>
       <Html className="h-full w-full">
         <Head>
@@ -36,13 +38,19 @@ export default function Email({
                 height={242}
               />
               <p className="text-lg">
-                <span className="font-bold text-xl">Nom:</span> {name}
+                <span className="font-bold text-xl">Nom:</span> {Nom}
+              </p>
+              <p className="text-lg">
+                <span className="font-bold text-xl">Prénom:</span> {Prénom}
               </p>
               <p className="text-lg">
                 <span className="font-bold text-xl">Téléphone:</span>{" "}
-                {phone_number}
+                {Téléphone}
               </p>
-              <p>{message}</p>
+              <p className="text-lg">
+                <span className="font-bold text-xl">Sujet:</span> {Sujet}
+              </p>
+              <p>{Message}</p>
             </Container>
           </Container>
         </body>
